f54482d7485137d40c33a3a6853aae5f8a158c8f
sharmapratham-96918 <prathams96918@gmail.com>
2026-03-14 15:35:09 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "",
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  plugins: [react(), tailwindcss()]
 })
